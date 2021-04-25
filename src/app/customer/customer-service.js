de03e4abd724e0574e0463c5d10067db510e5cb0
Felipe <felipeas314@gmail.com>
2021-04-25 13:53:32 -0300

    refatorando arquivos e pastas do projeto

---
+++ src/app/customer/customer-service.js
@@ -0,0 +1,21 @@
+const Customer = require('./customer-model');
+
+async function create(customer) {
+
+}
+
+async function index(page, size) {
+
+}
+
+async function findById(id) {
+
+}
+
+async function update(id, customer) {
+
+}
+
+async function destroy(id) {
+
+}
